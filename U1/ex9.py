33a89aca53be0c7b08a0197a5ebf671683532ff8
Guillermo <ramossantosguillermo14@gmail.com>
2025-04-02 08:22:53 +0200

    fixed ex9 and its documentations

--- U1/ex9.py
+++ U1/ex9.py
@@ -1,11 +1,11 @@
 n = int(input("Enter a number: "))
 
 def fact_sum(n):
-    sum = 0
-    if n==0:
+    sum = 0 # Initialize sum to 0
+    if n==0: # Base case: if n is 0, return 0
         return sum
-    else:
-        sum=n+fact_sum(n-1)
+    else: # Recursive case: add n to the sum of the fac_sum(n-1)
+        sum=n+fact_sum(n-1) 
     return sum
 
 print(fact_sum(n))
