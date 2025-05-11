9d66e0aac788866d0efb9b91f4a6440ad1dc18e8
Guillermo <ramossantosguillermo14@gmail.com>
2025-05-11 17:26:40 +0200

    Update ex7.py

--- U4/ex7.py
+++ U4/ex7.py
@@ -6,13 +6,13 @@ def find_max_shared_substring(A, B):
     L = [[0] * (n + 1) for _ in range(m + 1)]
 
     # Build the L array
-    for i in range(1, m + 1): # Iterate through each element of A
-        for j in range(1, n + 1): # Iterate through each element of B
+    for i in range(1,m + 1): # Iterate through each element of A
+        for j in range(1,n + 1): # Iterate through each element of B
             # If the current elements of A and B match, increment the length of the common subsequence
-            if A[i - 1] == B[j - 1]: 
+            if A[i-1] == B[j-1]:
                 L[i][j] = L[i - 1][j - 1] + 1 # Increment the length of the common subsequence
-            # If they do not match, take the maximum length from the previous elements
-            else:
+            
+            else:# If they do not match, take the maximum length from the previous elements
                 L[i][j] = max(L[i - 1][j], L[i][j - 1]) # The maximum length is the maximum of the two previous lengths (previous row or previous column)
 
     # Length of the longest common subsequence
@@ -22,7 +22,7 @@ def find_max_shared_substring(A, B):
     i, j = m, n
     common_subsequence = []
     while i > 0 and j > 0: # Backtrack through the L array
-        if A[i - 1] == B[j - 1]: # If the current elements of A and B match, it is part of the common subsequence
+        if L[i - 1][j] == L[i][j - 1]:
             common_subsequence.append(A[i - 1])
             i -= 1
             j -= 1
@@ -42,3 +42,5 @@ B = [1,0,1,0,0,1,0,0,1]
 l, s= find_max_shared_substring(A, B)
 print("Length of longest common subsequence:", l)
 print("Longest common subsequence:", s)
+
+
